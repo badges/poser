438341cdf1ee1b338de7ab325f4758e72e59a76a
jmleroux <jmleroux.pro@gmail.com>
2016-03-15 21:31:02 +0100

    scrutinizer integratio + Code cleaning + PHPSpec data provider

--- spec/PUGX/Poser/BadgeSpec.php
+++ spec/PUGX/Poser/BadgeSpec.php
@@ -5,20 +5,21 @@ namespace spec\PUGX\Poser;
 use PhpSpec\Exception\Exception;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use PUGX\Poser\Badge;
 
 class BadgeSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
-        $this->beConstructedWith('a','b','97CA00', 'svg');
+        $this->beConstructedWith('a', 'b', '97CA00', 'svg');
         $this->shouldHaveType('Pugx\Poser\Badge');
     }
 
     function it_should_be_constructed_by_fromURI_factory_method()
     {
-        $this->beConstructedWith('a','b','97CA00', 'svg');
+        $this->beConstructedWith('a', 'b', '97CA00', 'svg');
         $assert = 'version-stable-97CA00.svg';
-        $it = \PUGX\Poser\Badge::fromURI($assert);
+        $it = Badge::fromURI($assert);
 
         if ((string) $it !== $assert) {
             throw new Exception(sprintf("from[%s] having[%s]\n", $assert, (string) $it));
@@ -27,10 +28,10 @@ class BadgeSpec extends ObjectBehavior
 
     function it_should_be_constructed_by_fromURI_factory_method_escaping_correctly_underscores()
     {
-        $this->beConstructedWith('a','b','97CA00', 'svg');
+        $this->beConstructedWith('a', 'b', '97CA00', 'svg');
         $input = 'I__m__liugg__io-b-97CA00.svg';
         $assertInput = 'I_m_liugg_io-b-97CA00.svg';
-        $it = \PUGX\Poser\Badge::fromURI($input);
+        $it = Badge::fromURI($input);
 
         if ((string) $it !== $assertInput) {
             throw new Exception(sprintf("from[%s] wants[%s] having[%s]\n", $input, $assertInput, (string) $it));
@@ -39,10 +40,10 @@ class BadgeSpec extends ObjectBehavior
 
     function it_should_be_constructed_by_fromURI_factory_method_escaping_correctly_with_single_underscore()
     {
-        $this->beConstructedWith('a','b','97CA00', 'svg');
+        $this->beConstructedWith('a', 'b', '97CA00', 'svg');
         $input = 'I_m_liuggio-b-97CA00.svg';
         $assertInput = 'I m liuggio-b-97CA00.svg';
-        $it = \PUGX\Poser\Badge::fromURI($input);
+        $it = Badge::fromURI($input);
 
         if ((string) $it !== $assertInput) {
             throw new Exception(sprintf("from[%s] wants[%s] having[%s]\n", $input, $assertInput, (string) $it));
@@ -51,23 +52,34 @@ class BadgeSpec extends ObjectBehavior
 
     function it_should_be_constructed_by_fromURI_factory_method_escaping_correctly_with_dashes()
     {
-        $this->beConstructedWith('a','b','97CA00', 'svg');
+        $this->beConstructedWith('a', 'b', '97CA00', 'svg');
         $input = 'I--m--liuggio-b-97CA00.svg';
         $assertInput = 'I-m-liuggio-b-97CA00.svg';
-        $it = \PUGX\Poser\Badge::fromURI($input);
+        $it = Badge::fromURI($input);
 
         if ((string) $it !== $assertInput) {
             throw new Exception(sprintf("from[%s] wants[%s] having[%s]\n", $input, $assertInput, (string) $it));
         }
     }
 
-    function it_should_validate_available_color_schemes()
+    /**
+     * @dataProvider positiveConversionExamples
+     */
+    function it_should_validate_available_color_schemes($colorName, $expectedValue)
     {
-        $colorSchemes = \PUGX\Poser\Badge::getColorNamesAvailable();
+        $this->beConstructedWith('a', 'b', $colorName, 'svg');
+        $this->getHexColor()->shouldBeString();
+    }
 
-        foreach ($colorSchemes as $colorScheme) {
-            $this->beConstructedWith('a', 'b', $colorScheme, 'svg');
-            $this->getHexColor()->shouldBeString();
+    public function positiveConversionExamples()
+    {
+        $colorNames = Badge::getColorNamesAvailable();
+
+        $data = array();
+        foreach ($colorNames as $colorName) {
+            $data[] = array($colorName, null);
         }
+
+        return $data;
     }
 }
