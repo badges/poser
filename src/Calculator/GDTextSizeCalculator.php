f5db562c2e7ff5657d6fdc533f33950d59c302cd
jean-marie leroux <jmleroux.pro@gmail.com>
2017-02-11 16:11:56 +0100

    Rework travis file (#22)
    Rework travis file:
    * remove composer update because it is done automatically by Travis now
    * remove xdebug without test on PHP version to be more versatile
    * Update composer.lock
    * Add php-cs-fixer
    * Apply PSR2 fixes
    * Ad composer cache

--- src/Calculator/GDTextSizeCalculator.php
+++ src/Calculator/GDTextSizeCalculator.php
@@ -35,7 +35,7 @@ class GDTextSizeCalculator implements TextSizeCalculatorInterface
         $size = $this->convertToPt($size);
         $box  = imagettfbbox($size, 0, $this->fontPath, $text);
 
-        return round(abs($box[2] - $box[0]) + self::SHIELD_PADDING_EXTERNAL + self::SHIELD_PADDING_INTERNAL,  1);
+        return round(abs($box[2] - $box[0]) + self::SHIELD_PADDING_EXTERNAL + self::SHIELD_PADDING_INTERNAL, 1);
     }
 
     private function convertToPt($pixels)
--- src/Image.php
+++ src/Image.php
@@ -66,5 +66,4 @@ class Image
     {
         return $this->format;
     }
-
 }
