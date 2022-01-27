2d20de5ff82de1a5c60920db87369fe4adc09280
Ricardo Boss <contact@ricardoboss.de>
2022-01-27 15:08:09 +0100

    Fixed CS

--- src/Render/SvgForTheBadgeRenderer.php
+++ src/Render/SvgForTheBadgeRenderer.php
@@ -17,13 +17,13 @@ use PUGX\Poser\Calculator\TextSizeCalculatorInterface;
 
 class SvgForTheBadgeRenderer extends LocalSvgRenderer
 {
-    public const VENDOR_TEXT_FONT = __DIR__ . "/../Calculator/Font/RobotoMedium.svg";
-    public const VALUE_TEXT_FONT = __DIR__ . "/../Calculator/Font/MontserratExtraBold.svg";
-    public const TEXT_FONT_SIZE = 11;
-    public const TEXT_FONT_COLOR = '#FFFFFF';
+    public const VENDOR_TEXT_FONT    = __DIR__ . '/../Calculator/Font/RobotoMedium.svg';
+    public const VALUE_TEXT_FONT     = __DIR__ . '/../Calculator/Font/MontserratExtraBold.svg';
+    public const TEXT_FONT_SIZE      = 11;
+    public const TEXT_FONT_COLOR     = '#FFFFFF';
     public const TEXT_LETTER_SPACING = 0.1;
-    public const PADDING_X = 12;
-    public const Y_OFFSET_ROBOTO = 2;
+    public const PADDING_X           = 12;
+    public const Y_OFFSET_ROBOTO     = 2;
 
     private EasySVG $easy;
 
@@ -34,7 +34,7 @@ class SvgForTheBadgeRenderer extends LocalSvgRenderer
     ) {
         parent::__construct($textSizeCalculator, $templatesDirectory);
 
-        if ($easySVG === null) {
+        if (null === $easySVG) {
             $easySVG = new EasySVG();
         }
 
@@ -43,7 +43,7 @@ class SvgForTheBadgeRenderer extends LocalSvgRenderer
 
     public function getBadgeStyle(): string
     {
-        return "for-the-badge";
+        return 'for-the-badge';
     }
 
     protected function getTemplateName(): string
@@ -55,24 +55,24 @@ class SvgForTheBadgeRenderer extends LocalSvgRenderer
     {
         $parameters = parent::buildParameters($badge);
 
-        $parameters['vendor'] = mb_strtoupper($parameters['vendor']);
-        $parameters['value'] = mb_strtoupper($parameters['value']);
+        $parameters['vendor'] = \mb_strtoupper($parameters['vendor']);
+        $parameters['value']  = \mb_strtoupper($parameters['value']);
 
         $this->easy->clearSVG();
         $this->easy->setLetterSpacing(self::TEXT_LETTER_SPACING);
         $this->easy->setFont(self::VENDOR_TEXT_FONT, self::TEXT_FONT_SIZE, self::TEXT_FONT_COLOR);
-        $vendorDimensions = $this->easy->textDimensions($parameters['vendor']);
-        $parameters['vendorWidth'] = $vendorDimensions[0] + 2 * self::PADDING_X;
+        $vendorDimensions                      = $this->easy->textDimensions($parameters['vendor']);
+        $parameters['vendorWidth']             = $vendorDimensions[0] + 2 * self::PADDING_X;
         $parameters['vendorTextStartPosition'] = self::PADDING_X;
-        $parameters['vendorTextPath'] = $this->easy->addText($parameters['vendor'], $parameters['vendorTextStartPosition'], self::TEXT_FONT_SIZE / 2 + self::Y_OFFSET_ROBOTO)->asXML();
+        $parameters['vendorTextPath']          = $this->easy->addText($parameters['vendor'], $parameters['vendorTextStartPosition'], self::TEXT_FONT_SIZE / 2 + self::Y_OFFSET_ROBOTO)->asXML();
 
         $this->easy->clearSVG();
         $this->easy->setLetterSpacing(self::TEXT_LETTER_SPACING);
         $this->easy->setFont(self::VALUE_TEXT_FONT, self::TEXT_FONT_SIZE, self::TEXT_FONT_COLOR);
-        $valueDimensions = $this->easy->textDimensions($parameters['value']);
-        $parameters['valueWidth'] = $valueDimensions[0] + 2 * self::PADDING_X;
+        $valueDimensions                      = $this->easy->textDimensions($parameters['value']);
+        $parameters['valueWidth']             = $valueDimensions[0] + 2 * self::PADDING_X;
         $parameters['valueTextStartPosition'] = $parameters['vendorWidth'] + self::PADDING_X;
-        $parameters['valueTextPath'] = $this->easy->addText($parameters['value'], $parameters['valueTextStartPosition'], self::TEXT_FONT_SIZE / 2)->asXML();
+        $parameters['valueTextPath']          = $this->easy->addText($parameters['value'], $parameters['valueTextStartPosition'], self::TEXT_FONT_SIZE / 2)->asXML();
 
         $parameters['totalWidth'] = $parameters['valueWidth'] + $parameters['vendorWidth'];
 
